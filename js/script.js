9838ca4d3e66e6957c0d6b48c47e5c8c9ee02f7d
te260ku <teturou@aw.wakwak.com>
2021-01-28 10:37:37 +0900

    qrコードを読み込む処理を追加

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const video  = document.querySelector('#player')
+const canvas = document.querySelector('#snapshot')
+const ctx = canvas.getContext('2d')
 
 let handleSuccess = function(stream) {
     video.srcObject = stream;
@@ -13,3 +15,23 @@ navigator.mediaDevices
     .catch(function(err) {
         alert('Error!!')
     })
+
+const checkImage = () => {
+    // 取得している動画をCanvasに描画
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+    // Canvasからデータを取得
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
+    // jsQRに渡す
+    const code = jsQR(imageData.data, canvas.width, canvas.height)
+
+    // QRコードの読み取りに成功したらモーダル開く
+    // 失敗したら再度実行
+    if (code) {
+        // openModal(code.data)
+        alert('detected')
+    } else {
+        setTimeout(() => { checkImage() }, 500)
+    }
+}
